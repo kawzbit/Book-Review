429bdbbea7253d512e0b289987d9b90cea080ae9
Abdulhakeem Hamza <kawzbit01@gmail.com>
2022-07-27 09:04:52 +0100

    acoounts added

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.shortcuts import redirect
+from django.db import IntegrityError
+
+def signupaccount(request):
+
+    if request.method == 'GET':
+        return render(request,'signupaccount.html',{'form':UserCreationForm})
+    else:
+        try:
+            if request.POST['password1'] == request.POST['password2']:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home') 
+            else:
+                return render(request, 'signupaccount.html',{'form':UserCreationForm,'error':'Passwords do not match'})
+        except IntegrityError:
+            return render(request,'signupaccount.html',{'form':UserCreationForm, 'error': "User already exists. Choose a new username"})
+
+
+
+
--- bookreviews/urls.py
+++ bookreviews/urls.py
@@ -25,11 +25,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', bookViews.home, name = "home"),
-    path('about/', bookViews.about, name = "about"),
-    path('signup/', bookViews.signup, name = "signup"),
+    path('', bookViews.home, name = 'home'),
+    path('about/', bookViews.about, name = 'about'),
+    path('signup/', bookViews.signup, name = 'signup'),
     path('news/', include('news.urls')),
     path('book/', include('book.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
